71a76adb0f8ae199f9bfdf7f2602ae9840ceaf37
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2024-01-13 23:10:11 +0530

    Fix CI build failed Lint error

--- src/AuthGuard.tsx
+++ src/AuthGuard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { StrictMode, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import Keycloak from 'keycloak-js'
