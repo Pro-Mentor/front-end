8dad38f3fc18feaeff0fa6e62fbedf4bdb086bfd
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2024-04-13 00:21:00 +0530

    Add staff table data search

--- src/hooks/uni-admin/staff/useGetStaffTableDetails.tsx
+++ src/hooks/uni-admin/staff/useGetStaffTableDetails.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
 import { AuthService } from '../../../services/api/auth-service.api.endpoints'
 import { useCustomSWR } from '../../../services/useCustomSWR'
-import { GetResourceManagersResponse, Group } from '@promentor-app/shared-lib'
+import { GetResourceManagersResponse } from '@promentor-app/shared-lib'
 
 const api = new AuthService()
 
 export const useGetStaffTableDetails = () => {
-	const { data, error, isLoading, isValidating, mutate, customMutate } =
-		useCustomSWR<unknown, GetResourceManagersResponse[]>(api.Get_Staff(), 'GET')
+	const [search, setSearch] = useState('')
+	const { data, error, isLoading, isValidating, customMutate } = useCustomSWR<
+		unknown,
+		GetResourceManagersResponse[]
+	>(
+		api.Get_Staff({
+			search: search,
+		}),
+		'GET'
+	)
 
 	return {
 		getStaffResponse: data,
@@ -14,5 +23,6 @@ export const useGetStaffTableDetails = () => {
 		isLoading_getStaff: isLoading,
 		isValidating_getStaff: isValidating,
 		mutate_getStaff: customMutate,
+		setSearch_getStaff: setSearch,
 	}
 }
